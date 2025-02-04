bf7bf6eafd201f4793171512f837fbb9a062bfa6
Zen0092 <zgrayzen@gmail.com>
2025-02-04 09:32:18 +0800

    Forms.py

--- Forms.py
+++ Forms.py
@@ -1,5 +1,5 @@
-from wtforms import Form, StringField, TextAreaField, DecimalField, validators,SubmitField
-
+from wtforms import Form, StringField, TextAreaField, DecimalField, validators,SubmitField,EmailField, PasswordField
+from wtforms.validators import DataRequired, Email, EqualTo, Length
 class createProductsForm(Form):
     Product_name = StringField('Product Name', [validators.DataRequired()])
     Price = DecimalField('Price', [validators.NumberRange(min=0), validators.DataRequired(message="Price is required")])
@@ -11,3 +11,28 @@ class updateProductsForm(Form):
     Price = DecimalField('Price', [validators.NumberRange(min=0), validators.DataRequired()])
     Description = TextAreaField('Description', [validators.DataRequired()])
     submit = SubmitField('Update Product')
+
+# phyos starts here
+
+class CreateUserForm(Form):
+    username = StringField('Username', [validators.Length(min=1, max=150), validators.DataRequired()])
+    email = EmailField('Email', [validators.Email(message='Invalid Email Address'), validators.DataRequired()])
+    password = PasswordField('Password', [validators.Length(min=8, message="Passwords must be at least 8 characters."),
+                                          validators.DataRequired()])
+    submit = SubmitField('Sign Up')
+
+
+class LogInForm(Form):
+    email = EmailField('Email', [validators.Email(message='Invalid Email Address'), validators.DataRequired()])
+    password = PasswordField('Password', [validators.DataRequired()])
+    submit = SubmitField('Log in')
+
+
+class UpdatePasswordForm(Form):
+    current_password = PasswordField('Current Password', [validators.DataRequired()])
+    new_password = PasswordField('New Password', [validators.Length(min=8,
+                                                                    message="Passwords must be at least 8 characters."),
+                                                  validators.DataRequired()])
+    confirm_password = PasswordField('Confirm New Password', [validators.DataRequired(),
+                                                              validators.EqualTo('new_password', message='Password must match.'),])
+    submit = SubmitField('Update Password')
